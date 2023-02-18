d1ad70ad1734802706976e1b49b14089976f6924
marklum7 <vapilin777@gmail.com>
2023-02-18 11:13:42 +0300

    kk

---
+++ app/src/main/java/com/example/zz/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.zz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    private Integer counter = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClickBtnAddDog(View view) {
+        counter++;
+        TextView counterView = (TextView)findViewById(R.id.txt_counter);
+        counterView.setText(counter.toString());
+    }
+}
