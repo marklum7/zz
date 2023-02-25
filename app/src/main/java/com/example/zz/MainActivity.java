660613f2a4b6461c8f78bb9a48d882bb5daabc35
marklum7 <122746275+marklum7@users.noreply.github.com>
2023-02-25 09:49:12 +0300

    фикс

--- app/src/main/java/com/example/zz/MainActivity.java
+++ app/src/main/java/com/example/zz/MainActivity.java
@@ -3,20 +3,84 @@ package com.example.zz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
     private Integer counter = 0;
+    public static final String TAG = "StartActivity";
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (savedInstanceState != null &&
+                savedInstanceState.containsKey("count")) {
+            counter = savedInstanceState.getInt("count");
+        }
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate");
+        // Toast.makeText(this, "onCreate()", Toast.LENGTH_LONG).show();
     }
+
     public void onClickBtnAddDog(View view) {
         counter++;
         TextView counterView = (TextView)findViewById(R.id.txt_counter);
         counterView.setText(counter.toString());
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+        resetUI();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("count", counter);
+        Log.d(TAG, "onSaveInstanceState");
+
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null &&
+                savedInstanceState.containsKey("count")) {
+            counter = savedInstanceState.getInt("count");
+        }
+        Log.d(TAG, "onRestoreInstanceState");
+    }
+    private void resetUI() {
+        ((TextView) findViewById(R.id.txt_counter)).setText(counter.toString());
+        Log.d(TAG, "resetUI");
+    }
+
+
 }
